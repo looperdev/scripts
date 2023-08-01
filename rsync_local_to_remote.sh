cad4665765f5a6dfcab7759b4f29e27fdd491c46
Jonathan Looper <looperjp@gmail.com>
2023-08-01 08:52:28 -0500

    changed flag from a to r to handle permissions better

--- rsync_local_to_remote.sh
+++ rsync_local_to_remote.sh
@@ -1,3 +1,3 @@
 
 #dry run comparison between remote and local directory
-rsync -azvhP --dry-run --delete /mnt/c/work/ jlooper@waasccserv3:/user/home/jlooper/work
+rsync -rzvhP --dry-run --delete /mnt/c/work/ jlooper@waasccserv3:/user/home/jlooper/work
